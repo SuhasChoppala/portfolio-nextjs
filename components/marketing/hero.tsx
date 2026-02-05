d5176d1f688e5d41682f9c629fdac08adc6b756c
SuhasChoppala <choppalasuhas.02@gmail.com>
2026-02-05 18:10:50 +0530

    added img in hero section

--- components/marketing/hero.tsx
+++ components/marketing/hero.tsx
@@ -1,29 +1,43 @@
 "use client";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <>
-      <main>
-        <section className="overflow-hidden">
-          <div className="relative mx-auto max-w-5xl px-6 py-28 lg:py-20">
-            <div className="lg:flex lg:items-center">
-              <div className="relative z-10 mx-auto max-w-xl text-center lg:ml-0 lg:w-1/2 lg:text-left">
-                <div className="rounded-(--radius) bg-primary-foreground mx-auto flex w-fit items-center gap-2 border p-1 lg:ml-0">
-                  <h3 className="text-sm">Full Stack Developer</h3>
-                </div>
+    <main>
+      <section className="overflow-hidden">
+        <div className="mx-auto max-w-5xl px-6 py-24">
+          <div className="flex flex-col-reverse items-center gap-10 lg:flex-row lg:items-center">
+            {/* Text */}
+            <div className="text-center lg:w-1/2 lg:text-left">
+              <div className="mx-auto w-fit rounded border px-3 py-1 lg:mx-0">
+                <h3 className="text-sm">Full Stack Developer</h3>
+              </div>
 
-                <h1 className="mt-4 text-balance !leading-[1.25] tracking-tight text-4xl font-semibold md:text-5xl xl:text-5xl">
-                  Suhas Choppala
-                </h1>
-                <p className="mt-2 text-base text-muted-foreground">
-                  Crafting exceptional digital experiences with modern
-                  technologies and pixel-perfect design.
-                </p>
+              <h1 className="mt-4 text-4xl font-semibold tracking-tight md:text-5xl">
+                Suhas Choppala
+              </h1>
+
+              <p className="mt-2 text-base text-muted-foreground">
+                Crafting exceptional digital experiences with modern
+                technologies and pixel-perfect design.
+              </p>
+            </div>
+
+            {/* Image */}
+            <div className="lg:w-1/2 flex justify-center lg:justify-end">
+              <div className="relative h-[140px] w-[140px]">
+                <Image
+                  src="/profileImg.jpg"
+                  alt="Suhas Choppala"
+                  fill
+                  className="object-cover rounded-xl"
+                  priority
+                />
               </div>
             </div>
           </div>
-        </section>
-      </main>
-    </>
+        </div>
+      </section>
+    </main>
   );
 }
